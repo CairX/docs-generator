7b791f7ae5a715481ba3cb971f5136e1a66d1725
cairns <lazycairx@gmail.com>
2014-11-22 17:05:02 +0100

    WIP of rewriting.

--- generator.py
+++ generator.py
@@ -5,35 +5,61 @@ def generator():
     with open('test.js', 'r') as f:
         #within = False
         another = False
-        info = ''
+        lines = []
 
         for line in f:
             line = line.strip()
 
             if another:
-                info += line + '\n'
-                block(info)
-                info = ''
+                lines.append(line)
+                block(lines)
+                lines = []
                 another = False
 
             if line.startswith('/**'):
-                info += line + '\n'
+                lines.append(line)
                 #within = True
                 #print('something')
 
             elif line.endswith('*/'):
-                info += line + '\n'
+                lines.append(line)
                 #within = False
                 another = True
 
             elif line.startswith('*'):
-                #line = line[1:].strip()
-                info += line + '\n'
+                #line = line[1:]s.strip()
+                lines.append(line)
 
 
-def block(info):
+def block(lines):
+    testy = ''
+    for line in lines:
+        if line.startswith('/**'):
+            testy += line.replace('/**', '', 1).strip()
+        elif line.endswith('*/'):
+            testy += line[:-2]
+        elif line.startswith('*'):
+            testy += line.replace('*', '', 1).strip()
+
+    valid = ['@class', '@method', '@param', '@return']
+
+    for v in valid:
+        breaks = index(testy, v)
+
+    breaks.sort()
+
+    restult = {}
+    result['description'] = testy[:breaks[0]]
+    breaks = breaks[1:]
+
+    #for index in breaks:
+    #    ws =
+
+    print(testy)
+
+    """
     result = {}
-    for line in info.split('\n'):
+    for line in lines:
         print(line)
         if (line.startswith('*/') or line.startswith('/**')):
             continue
@@ -62,6 +88,19 @@ def block(info):
                     result['name'] = m2.group(2)
 
     print(result)
+    """
+
+
+def index(s, sub):
+    index = 0
+
+    while index < len(s):
+        index = s.find(sub, index)
+        if index == -1:
+            break
+
+        print(index)
+        index += len(sub)
 
 
 if __name__ == '__main__':
