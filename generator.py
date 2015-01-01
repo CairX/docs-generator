fd8dee4a0cb1c2b25f980c50fdcb126813d0eda6
cairns <lazycairx@gmail.com>
2015-01-01 19:26:59 +0100

    Basic output straight to the terminal.

--- generator.py
+++ generator.py
@@ -2,6 +2,61 @@ import index
 import re
 
 
+class Param(object):
+
+    def __init__(self, s):
+        match = re.match(r'(\w+) \{(\w+?)\} (.+)', s)
+
+        if match:
+            self.name = match.group(1)
+            self.type = match.group(2)
+            self.description = match.group(3)
+
+    def __str__(self):
+        result = '**' + self.name + '**'
+        result += ' *' + self.type + '*'
+        result += ' ' + self.description
+
+        return result
+
+
+class Section(object):
+
+    def __init__(self, parts):
+        self.params = []
+
+        for part in parts:
+            part = part.strip()
+
+            if part.startswith('@class'):
+                self.clazz = part.replace('@class', '').strip()
+
+            elif part.startswith('@method'):
+                self.method = part.replace('@method', '').strip()
+
+            elif part.startswith('@param'):
+                self.params.append(Param(part.replace('@param', '').strip()))
+
+            elif not part.startswith('@'):
+                self.description = part.strip()
+
+    def __str__(self):
+        result = ''
+        if hasattr(self, 'clazz'):
+            result += '# ' + self.clazz + '\n'
+
+        if hasattr(self, 'method'):
+            result += '## ' + self.method + '\n'
+
+        if hasattr(self, 'description'):
+            result += self.description + '\n\n'
+
+        for param in self.params:
+            result += '* ' + str(param) + '\n'
+
+        return result
+
+
 def generator():
     tags = ['@class', '@method', '@param', '@return']
 
@@ -13,8 +68,7 @@ def generator():
             positions = index.array(comment, tags)
             parts = index.split(comment, positions)
 
-            print(comment)
-            print(parts)
+            print(Section(parts))
 
 
 def cleanComment(comment):
@@ -24,29 +78,6 @@ def cleanComment(comment):
     comment = comment.strip()
 
     return comment
-
-
-def out(o):
-    result = ''
-
-    if '@class' in o:
-        result += '# ' + o['@class'] + '\n'
-
-    if '@method' in o:
-        result += '## ' + o['@method'] + '\n'
-
-    if 'description' in o:
-        result += o['description'] + '\n\n'
-
-    if '@param' in o:
-        for p in o['@param']:
-            m = re.match(r'(\w+) \{(\w+?)\} (.+)', p)
-            if m:
-                result += '**' + m.group(1) + '**'
-                result += ' *' + m.group(2) + '*'
-                result += ' ' + m.group(3) + '\n\n'
-
-    print(result)
 
 
 if __name__ == '__main__':
