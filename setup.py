5da16450626005043f5cb3a9b648b9c378529b48
cairns <lazycairx@gmail.com>
2015-01-09 11:51:59 +0100

    More information in the setup.

--- setup.py
+++ setup.py
@@ -2,11 +2,24 @@ from setuptools import setup
 
 setup(
     name='docs-generator',
+    description='Create markdown documentation based on comments in JavaScript files.',
+    keywords='generator documentation markdown javascript',
     version='0.1dev0',
+    author='Thomas Cairns',
+    url='https://github.com/CairX/docs-generator',
+
     packages=['docs_generator'],
     entry_points={
         'console_scripts': [
             'docs-generator=docs_generator:main',
         ]
-    }
+    },
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Documentation',
+    ]
 )
