9c6a4300630e708f5f78a5d3d4fd17dbcf6f9e5b
cairns <lazycairx@gmail.com>
2015-03-15 10:53:59 +0100

    Add the basics for project based config file.

--- docs_generator/__init__.py
+++ docs_generator/__init__.py
@@ -1,22 +1,30 @@
 from docs_generator import index
 from docs_generator.parts import Section, Comment
+from configparser import ConfigParser
 import re
 import sys
 
 
 def main():
     print('Generator')
-    generator(sys.argv[1:])
+    config = ConfigParser()
+    config.read('documentation.conf')
+    generator(sys.argv[1:], config['comments'])
 
 
-def generator(paths):
+def generator(paths, config):
+    pattern = re.escape(config.get('start', '/*').strip('"'))
+    pattern += '(.+?)'
+    pattern += re.escape(config.get('end', '*/').strip('"'))
+    # print(pattern)
+
     tags = ['@bind', '@method', '@param', '@return', '@section']
     sections = {}
 
     for path in paths:
         try:
             with open(path, 'r') as f:
-                comments = re.findall(r'(\/\*\*.+?\*\/)', f.read(), re.DOTALL)
+                comments = re.findall(pattern, f.read(), re.DOTALL)
 
                 for comment in comments:
                     comment = cleanComment(comment)
@@ -27,9 +35,13 @@ def generator(paths):
 
                     if title:
                         sections[title] = Section(title)
+                        print(title)
                     else:
-                        comment = Comment(parts)
-                        sections[comment.bind].add(comment)
+                        try:
+                            comment = Comment(parts)
+                            sections[comment.bind].add(comment)
+                        except KeyError:
+                            print('Missing bind.')
 
             print('Documentation done:', path)
         except FileNotFoundError:
@@ -42,8 +54,6 @@ def generator(paths):
 
 def cleanComment(comment):
     comment = re.sub(r'(\n.+\*\s+)', ' ', comment)
-    comment = comment.replace('/**', '', 1)
-    comment = comment.replace('*/', '')
     comment = comment.strip()
 
     return comment
