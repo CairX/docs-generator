cae24dc350a74d32ed94fb2daf43466dd0e4d326
cairns <lazycairx@gmail.com>
2015-03-15 20:28:43 +0100

    Extract an extra line after the comment section. This will later on be used to extract the method name from the actual definition instead of having to use a tag.

--- docs_generator/__init__.py
+++ docs_generator/__init__.py
@@ -13,9 +13,14 @@ def main():
 
 
 def generator(paths, config):
-    pattern = re.escape(config.get('start', '/*').strip('"'))
-    pattern += '(.+?)'
-    pattern += re.escape(config.get('end', '*/').strip('"'))
+    start = config.get('start', '/*').strip('"')
+    end = config.get('end', '*/').strip('"')
+
+    pattern = re.escape(start)
+    pattern += '\n[\s\S]*?'
+    pattern += re.escape(end)
+    pattern += '\n.*?'
+    print(pattern)
 
     tags = ['@bind', '@method', '@param', '@return', '@section']
     sections = []
@@ -23,10 +28,12 @@ def generator(paths, config):
     for path in paths:
         try:
             with open(path, 'r') as f:
-                comments = re.findall(pattern, f.read(), re.DOTALL)
+                comments = re.findall(pattern, f.read())
+                print(comments)
 
                 for comment in comments:
-                    comment = clean_comment(comment)
+                    comment = clean_comment(comment, start, end)
+                    print(comment)
                     positions = index.array(comment, tags)
                     parts = index.split(comment, positions)
 
@@ -51,7 +58,9 @@ def generator(paths, config):
             f.writelines(str(section) + '\n\n')
 
 
-def clean_comment(comment):
+def clean_comment(comment, start, end):
+    comment = comment.replace(start, '')
+    comment = comment.replace(end, '')
     comment = re.sub(r'(\n.+\*\s+)', ' ', comment)
     comment = comment.strip()
 
